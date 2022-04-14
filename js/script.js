d0c84f3eb45e32f6390194aa55dcab2810ec4129
LucidWinter <mike.brown11311@gmail.com>
2022-04-14 10:51:43 -0400

    Finished first part of script
    Computer will randomly select rock/paper/scissor,
    User can enter in rock/paper/scissor and comparison is case-insensitive,
    Outputs winner and what choices were picked

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-function computerPlay() {
-    let randomSelection = Math.floor(Math.random() * 3) + 1
-    let computerChoice = ''
+function userPlay() {
+    let userChoice = prompt('Please enter in Rock, Paper or Scissor')
 
-    if (randomSelection === 1) {
-        return computerChoice = 'rock'
-    } else if (randomSelection === 2) {
-        return computerChoice = 'paper'
-    } else {
-        return computerChoice = 'scissor'
+    return userChoice.toLowerCase()
+}
+
+function computerPlay() {
+    let rand = Math.floor(Math.random() * 3)
+    let computerChoice
+
+    switch (rand) {
+        case 0:
+            return computerChoice = 'rock'
+        case 1:
+            return computerChoice = 'paper'
+        case 2:
+            return computerChoice = 'scissor'
     }
 }
 
-console.log(computerPlay());
+function playGame(playerSelection, computerSelection) {
+    let winner
+
+    if (playerSelection === computerSelection) {
+        return winner = `You tied! You both selected ${playerSelection}`
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissor') ||
+        (playerSelection === 'scissor' && computerSelection === 'rock')) {
+
+        return winner = `You lost! ${computerSelection} beats ${playerSelection}`
+    } else {
+        return winner = `You win! ${playerSelection} beats ${computerSelection}`
+    }
+}
+
+const playerSelection = userPlay();
+const computerSelection = computerPlay();
+console.log(playGame(playerSelection, computerSelection))
+
